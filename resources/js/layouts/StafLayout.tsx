1c302a5e1dd23a6b79d9fcf181b06b331cc1c047
MabesP4W <mabesp4w@gmail.com>
2026-06-12 06:14:31 +0900

    build: refresh production assets and update StafLayout component

--- resources/js/layouts/StafLayout.tsx
+++ resources/js/layouts/StafLayout.tsx
@@ -57,7 +57,7 @@ const menuItems = [
     },
     { type: 'title' as const, label: 'Layanan Surat' },
     {
-        label: 'Pengajuan Masuk',
+        label: 'Pengajuan Surat',
         href: '/staf/pengajuan',
         icon: (
             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
